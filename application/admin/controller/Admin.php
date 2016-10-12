64265866c44ba74904c8dc482e8a5dbca4e2979c
ljh <504429906@qq.com>
2016-10-12 17:22:21 +0800

    .

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Uctoo-Near
+ * Date: 2016/10/12
+ * Time: 10:17
+ */
+namespace app\admin\controller;
+
+class Admin {
+    public function index(){
+            echo 111;
+            return [1,2,3,4,5];
+    }
+
+
+
+
+}
